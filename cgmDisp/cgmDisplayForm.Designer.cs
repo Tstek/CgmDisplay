4f16b789d787a3e8cbf8ee68083e27893125d218
Tstek <sjpost@ucdavis.edu>
2025-09-20 15:34:19 -0700

    Convert to nightscout, change layout

---
+++ cgmDisp/cgmDisplayForm.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace cgmDisp
+{
+    partial class cgmDisplayForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(cgmDisplayForm));
+            this.panelBackGround = new System.Windows.Forms.Panel();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.labelDelta = new System.Windows.Forms.Label();
+            this.labelGlucose = new System.Windows.Forms.Label();
+            this.bgGraph = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panelBackGround.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bgGraph)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelBackGround
+            // 
+            this.panelBackGround.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelBackGround.Controls.Add(this.bgGraph);
+            this.panelBackGround.Controls.Add(this.labelTime);
+            this.panelBackGround.Controls.Add(this.labelDelta);
+            this.panelBackGround.Controls.Add(this.labelGlucose);
+            this.panelBackGround.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelBackGround.ForeColor = System.Drawing.Color.Black;
+            this.panelBackGround.Location = new System.Drawing.Point(0, 0);
+            this.panelBackGround.Name = "panelBackGround";
+            this.panelBackGround.Size = new System.Drawing.Size(317, 182);
+            this.panelBackGround.TabIndex = 0;
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTime.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(151)))), ((int)(((byte)(31)))));
+            this.labelTime.Location = new System.Drawing.Point(11, 23);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(72, 19);
+            this.labelTime.TabIndex = 6;
+            this.labelTime.Text = "12:00AM";
+            this.labelTime.Click += new System.EventHandler(this.labelTime_Click);
+            // 
+            // labelDelta
+            // 
+            this.labelDelta.AutoSize = true;
+            this.labelDelta.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDelta.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(151)))), ((int)(((byte)(31)))));
+            this.labelDelta.Location = new System.Drawing.Point(227, 15);
+            this.labelDelta.Name = "labelDelta";
+            this.labelDelta.Size = new System.Drawing.Size(44, 32);
+            this.labelDelta.TabIndex = 5;
+            this.labelDelta.Text = "+0";
+            // 
+            // labelGlucose
+            // 
+            this.labelGlucose.AutoSize = true;
+            this.labelGlucose.Font = new System.Drawing.Font("Consolas", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelGlucose.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(151)))), ((int)(((byte)(31)))));
+            this.labelGlucose.Location = new System.Drawing.Point(110, 11);
+            this.labelGlucose.Name = "labelGlucose";
+            this.labelGlucose.Size = new System.Drawing.Size(113, 41);
+            this.labelGlucose.TabIndex = 4;
+            this.labelGlucose.Text = "--- →";
+            // 
+            // bgGraph
+            // 
+            this.bgGraph.BackColor = System.Drawing.Color.Transparent;
+            this.bgGraph.BorderlineColor = System.Drawing.Color.Transparent;
+            this.bgGraph.BorderSkin.PageColor = System.Drawing.Color.Transparent;
+            chartArea1.AxisX.LabelStyle.ForeColor = System.Drawing.Color.Gray;
+            chartArea1.AxisX.LineColor = System.Drawing.Color.Gray;
+            chartArea1.AxisX.MajorGrid.LineColor = System.Drawing.Color.Gray;
+            chartArea1.AxisX.MajorTickMark.LineColor = System.Drawing.Color.WhiteSmoke;
+            chartArea1.AxisX.MinorGrid.LineColor = System.Drawing.Color.WhiteSmoke;
+            chartArea1.AxisX.MinorTickMark.LineColor = System.Drawing.Color.WhiteSmoke;
+            chartArea1.AxisX.TitleForeColor = System.Drawing.Color.Gray;
+            chartArea1.AxisY.LabelStyle.ForeColor = System.Drawing.Color.Gray;
+            chartArea1.AxisY.LabelStyle.TruncatedLabels = true;
+            chartArea1.AxisY.LineColor = System.Drawing.Color.Gray;
+            chartArea1.AxisY.MajorGrid.LineColor = System.Drawing.Color.Gray;
+            chartArea1.AxisY.TitleForeColor = System.Drawing.Color.Gray;
+            chartArea1.BackColor = System.Drawing.Color.Transparent;
+            chartArea1.Name = "ChartArea1";
+            this.bgGraph.ChartAreas.Add(chartArea1);
+            this.bgGraph.Location = new System.Drawing.Point(-7, 53);
+            this.bgGraph.Name = "bgGraph";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastPoint;
+            series1.Color = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(151)))), ((int)(((byte)(31)))));
+            series1.Name = "bgPoints";
+            series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Time;
+            series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            this.bgGraph.Series.Add(series1);
+            this.bgGraph.Size = new System.Drawing.Size(323, 128);
+            this.bgGraph.TabIndex = 7;
+            this.bgGraph.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(40)))), ((int)(((byte)(34)))));
+            this.ClientSize = new System.Drawing.Size(317, 182);
+            this.Controls.Add(this.panelBackGround);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CGM Display";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelBackGround.ResumeLayout(false);
+            this.panelBackGround.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bgGraph)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelBackGround;
+        private System.Windows.Forms.Label labelGlucose;
+        private System.Windows.Forms.Label labelDelta;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.DataVisualization.Charting.Chart bgGraph;
+    }
+}
+
---
+++ cgmDisp/cgmDisplayForm.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Linq;
+using System.Drawing;
+
+//Icons made by 'Pixel perfect' from www.flaticon.com
+
+namespace cgmDisp
+{
+    public partial class cgmDisplayForm : Form
+    {
+        public cgmDisplayForm()
+        {
+            InitializeComponent();
+            try
+            {
+                string configJson = File.ReadAllText(".//nightscoutConfig.json");
+
+                NightscoutConfig config = JsonConvert.DeserializeObject<NightscoutConfig>(configJson);
+                nightscout = new NightscoutAPI(config.baseUrl, config.token);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(string.Format("{0}\n{1}", "Error: Could not read nightscoutConfig.json", e.Message));
+            }
+
+            bgGraph.MouseDown += PanelBackGround_MouseDown;
+            panelBackGround.MouseDown += PanelBackGround_MouseDown;
+            panelBackGround.Select();
+
+            trendArrows.Add("Flat", "→");
+            trendArrows.Add("SingleUp", "↑");
+            trendArrows.Add("SingleDown", "↓");
+            trendArrows.Add("FortyFiveUp", "↗");
+            trendArrows.Add("FortyFiveDown", "↘");
+            trendArrows.Add("DoubleUp", "↑↑");
+            trendArrows.Add("DoubleDown", "↓↓");
+
+            //initial values
+
+            string resp = nightscout.GetLatest(36); //36 data points should be 3hrs
+            if (!string.IsNullOrWhiteSpace(resp))
+            {
+                CgmEntry[] entries = JsonConvert.DeserializeObject<CgmEntry[]>(resp);
+                BgValues = new List<int>();
+                BgTimes = new List<DateTime>();
+
+                for (int i = entries.Length - 1; i >= 0; i--)//points are in reverse order
+                {
+                    BgValues.Add(entries[i].sgv);
+                    BgTimes.Add(DateTimeOffset.Parse(entries[i].dateString).LocalDateTime);
+                }
+                SetGraph(bgGraph, BgTimes, BgValues);
+            }
+
+            _updateThread = new Thread(GetVals);
+            _updateThread.IsBackground = true;
+            _updateThread.Start();
+        }
+        private List<int> BgValues;
+        private List<DateTime> BgTimes;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (nightscout == null)
+            {
+                this.Close();
+            }
+        }
+
+        NightscoutAPI nightscout = null;
+        private Dictionary<string, string> trendArrows = new Dictionary<string, string>();
+        private Thread _updateThread;
+    
+        private void GetVals()
+        {            
+            while (true)
+            {
+                try
+                {
+                    DateTime nextRead = DateTime.Now.AddMinutes(1.0); //just in case the http call fails/junk is returned (if nightscout is down or something we'll only try every minute instead of every 5 seconds
+                    try
+                    {
+                        string resp = nightscout.GetLatest(1);
+                        if (!string.IsNullOrWhiteSpace(resp))
+                        {
+                            CgmEntry[] entries = JsonConvert.DeserializeObject<CgmEntry[]>(resp);
+                            addVal(entries[0]);
+                            DateTime dataTime = DateTime.Parse(entries[0].dateString);
+                            nextRead = dataTime.AddMinutes(5.3); //seems to be more accurate to when info is available than the website by a few seconds, without needing to check each 5 secs
+                        }
+                    }
+                    catch
+                    {
+                        //guess it failed! probably home assistant is down/bad connection.
+                    }
+                    if (nextRead < DateTime.Now)
+                        nextRead = DateTime.Now.AddSeconds(5.0); //if we checked 5 mins after last reading and it hasnt updated yet, check again in 5 seconds
+                    Thread.Sleep(nextRead - DateTime.Now);
+                }
+                catch { /* really hasn't been an issue if this fails occasionally, just dont break */ }
+            }
+        }
+        private void addVal(CgmEntry data)
+        {
+            SetText(labelGlucose, string.Format("{0} {1}", data.sgv, trendArrows[data.direction])); //↓↘↑⇈⇊
+            SetText(labelDelta, string.Format("{0}{1}", (data.delta > 0 ? "+" : ""), data.delta.ToString("0.0")));
+            SetText(labelTime, DateTimeOffset.Parse(data.dateString).LocalDateTime.ToShortTimeString());
+            BgValues.Add(data.sgv);
+            BgTimes.Add(DateTimeOffset.Parse(data.dateString).LocalDateTime);
+            int removed = BgValues[0]; 
+            BgValues.RemoveAt(0);
+            BgTimes.RemoveAt(0);
+            AddGraphVal(bgGraph, BgTimes.Last(), BgValues.Last(), removed >= (int)category.high ? category.high : (removed <= (int)category.low ? category.low : category.mid));
+        }
+
+        #region UI
+        delegate void dSetText(Control ctrl, string text);
+        private void SetText(Control ctrl, string text)
+        {
+            if (ctrl.InvokeRequired)
+            {
+                dSetText dst = new dSetText(SetText);
+                ctrl.Invoke(dst, new object[] { ctrl, text });
+            }
+            else
+            {
+                ctrl.Text = text;
+            }
+        }
+        enum category
+        {
+            mid = 0,
+            high = 170,
+            low = 70
+        }
+        delegate void dSetGraph(Chart ctrl, List<DateTime> times, List<int> values);
+        private void SetGraph(Chart chart,  List<DateTime> times, List<int> values)
+        {
+            if (chart.InvokeRequired)
+            {
+                dSetGraph dsg = new dSetGraph(SetGraph);
+                chart.Invoke(dsg, new object[] { chart, times, values });
+            }
+            else
+            {
+                chart.Series.Clear();
+                chart.Series.Add(category.high.ToString()).Color = Color.FromArgb(253, 151, 31);
+                chart.Series.Add(category.mid.ToString()).Color = Color.FromArgb(102, 217, 239);
+                chart.Series.Add(category.low.ToString()).Color = Color.FromArgb(249, 38, 114);
+                chart.Series[category.high.ToString()].ChartType = SeriesChartType.FastPoint;
+                chart.Series[category.mid.ToString()].ChartType = SeriesChartType.FastPoint;
+                chart.Series[category.low.ToString()].ChartType = SeriesChartType.FastPoint;
+                chart.Series[category.high.ToString()].MarkerStyle = MarkerStyle.Circle;
+                chart.Series[category.mid.ToString()].MarkerStyle = MarkerStyle.Circle;
+                chart.Series[category.low.ToString()].MarkerStyle = MarkerStyle.Circle;
+                chart.Series[category.high.ToString()].XValueType = ChartValueType.Time;
+                chart.Series[category.mid.ToString()].XValueType = ChartValueType.Time;
+                chart.Series[category.low.ToString()].XValueType = ChartValueType.Time;
+                chart.Series[category.high.ToString()].YValueType = ChartValueType.Int32;
+                chart.Series[category.mid.ToString()].YValueType = ChartValueType.Int32;
+                chart.Series[category.low.ToString()].YValueType = ChartValueType.Int32;
+
+                for (int i = 0; i < values.Count; i++)
+                {
+                    if (values[i] >= (int)category.high)
+                    {
+                        chart.Series[category.high.ToString()].Points.AddXY(times[i], values[i]);
+                    } 
+                    else if (values[i] <= (int)category.low)
+                    {
+                        chart.Series[category.low.ToString()].Points.AddXY(times[i], values[i]);
+                    } 
+                    else
+                    {
+                        chart.Series[category.mid.ToString()].Points.AddXY(times[i], values[i]);
+                    }
+                }
+                chart.ChartAreas[0].AxisY.Maximum = Math.Max((values.Max() + 10), 200);
+                chart.ChartAreas[0].AxisY.Minimum = Math.Min((values.Min() - 10), 80);
+                chart.ChartAreas[0].RecalculateAxesScale();
+
+            }
+        }
+        delegate void dAddGraphVal(Chart ctrl, DateTime time, int value, category removeFrom);
+        private void AddGraphVal(Chart chart, DateTime time, int value, category removeFrom)
+        {
+            if (chart.InvokeRequired)
+            {
+                dAddGraphVal dsg = new dAddGraphVal(AddGraphVal);
+                chart.Invoke(dsg, new object[] { chart, time, value, removeFrom });
+            }
+            else
+            {
+                chart.Series[removeFrom.ToString()].Points.RemoveAt(0);
+                if (value >= (int)category.high)
+                {
+                    chart.Series[category.high.ToString()].Points.AddXY(time, value);
+                }
+                else if (value <= (int)category.low)
+                {
+                    chart.Series[category.low.ToString()].Points.AddXY(time, value);
+                }
+                else
+                {
+                    chart.Series[category.mid.ToString()].Points.AddXY(time, value);
+                }
+                chart.ChartAreas[0].AxisY.Maximum = (BgValues.Max() + 5);
+                chart.ChartAreas[0].AxisY.Minimum = (BgValues.Min() - 5);
+                chart.ChartAreas[0].RecalculateAxesScale();
+            }
+        }
+        //this lets us hide the windows title bar (mostly just to make things smaller/mesh better in VR using XSOverlay)
+        //also toggles always on top mode
+        private static FormBorderStyle fbs = FormBorderStyle.None;
+        private void labelTime_Click(object sender, EventArgs e)
+        {
+            TopMost = !TopMost;
+            if (this.FormBorderStyle != FormBorderStyle.None)
+            {
+                fbs = this.FormBorderStyle;
+                this.FormBorderStyle = FormBorderStyle.None;
+            }
+            else
+            {
+                this.FormBorderStyle = fbs;
+            }
+        }
+        #endregion UI
+        #region MoveWithBackground
+        //this lets us still move around the window while the titlebar is hidden by clicking and dragging the background
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void PanelBackGround_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+        #endregion MoveWithBackground
+    }
+}
